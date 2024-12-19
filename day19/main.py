0606a4d870240da22cc72da63b9ab9ddc725d93e
Sam Hine <sam.hine27@gmail.com>
2024-12-19 16:42:54 +0000

    feat: rework, p2

--- day19/main.py
+++ day19/main.py
@@ -1,55 +1,44 @@
-import itertools
-from collections import defaultdict
+from functools import cache
 
 with open("input.txt", "r") as file:
     input = file.read().splitlines()
-    available = input[0].split(", ")
-    patterns = input[2:]
+    patterns = tuple(input[0].split(", "))
+    displays = input[2:]
 
-print(available)
 print(patterns)
+print(displays)
 
-# Go through pattern with pointers, if we find match between our pointers, save it, and continue until pointers have scanned entire string
-# Then we go through our saved matches, and repeat the process
-# If our saved matches == the string 
+@cache
+def check_pattern_possible(display, patterns):
+    if display in patterns:
+        return True
+    possible = False
+    for pattern in patterns:
+        if display.startswith(pattern):
+            if check_pattern_possible(display[len(pattern):], patterns):
+                possible = True
+                break
+    return possible
 
-def find_matches(string, current_matches, completed=[]):
-    #print(string, current_matches)
-    next_matches = []
-    accounted = []
-    for match in current_matches:
-        coagulated = ''.join(match)
-        if string == coagulated:
-            completed.append(match)
-            continue
-        
-        # Align pointers to next character after match in string
-        idx = [idx for idx, val in enumerate(string) if string[:idx] == coagulated][0]
-        lp = idx
-        rp = idx+1
+@cache
+def count_possible_patterns(display, patterns):
+    possible = 0
+    if display in patterns:
+        possible += 1
+    for pattern in patterns:
+        if display.startswith(pattern):
+            possible += count_possible_patterns(display[len(pattern):], patterns)
+    return possible
 
-        while rp <= len(string):
-            if string[lp:rp] in available:
-                #print("Found", string[lp:rp])
-                new_match = match.copy()
-                new_match.append(string[lp:rp])
-                new_coagulated = ''.join(new_match)
-                if new_coagulated not in accounted: # Do not append duplicates
-                    accounted.append(new_coagulated)
-                    next_matches.append(new_match)
-                
-            rp += 1
+possible = 0
+for display in displays:
+    if check_pattern_possible(display, patterns):
+        possible += 1
 
-    if len(next_matches) == 0:
-        print(completed)
-        return len(completed)
-    
-    return find_matches(string, next_matches, completed)
+print(possible)
 
-found = 0
-for pattern in patterns:
-    print(pattern)
-    found += find_matches(pattern, [[""]], [])
+count = 0
+for display in displays:
+    count += count_possible_patterns(display, patterns)
 
-print(found)
-
+print(count)
